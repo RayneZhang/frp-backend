fe42b7b4487d89190ed68549e800396116da3f24
RayneZ <rayzhange@hotmail.com>
2019-08-08 11:22:50 -0400

    export Scene for front-end

--- built/index.js
+++ built/index.js
@@ -1,22 +1,23 @@
 "use strict";
 exports.__esModule = true;
-//export { Scene } from './Scene';
 var Scene_1 = require("./Scene");
-var scene = new Scene_1.Scene();
-var a = scene.addConstant(2000);
-// const gen = scene.addOp('gen');
-// scene.addEdge(a, {node: gen, prop: 'delay'})
-var b = scene.addConstant(10);
-var p = scene.addOp('+');
-scene.addEdge(b, p);
-scene.addEdge(a, p);
-p.pluckOutput().subscribe(function (value) {
-    console.log("add output is", value);
-});
-var cube = scene.addObj('cube', [{ name: 'color', "default": 'red' }]);
-var sphere = scene.addObj('sphere', [{ name: 'color', "default": 'blue' }]);
-sphere.pluckOutput('color').subscribe(function (value) {
-    console.log("sphere output is", value);
-});
-scene.addEdge({ node: cube, prop: 'color' }, { node: sphere, prop: 'color' });
+exports.Scene = Scene_1.Scene;
+// import { Scene } from './Scene';
+// const scene = new Scene();
+// const a = scene.addConstant(2000);
+// // const gen = scene.addOp('gen');
+// // scene.addEdge(a, {node: gen, prop: 'delay'})
+// const b = scene.addConstant(10);
+// const p = scene.addOp('+');
+// scene.addEdge(b, p);
+// scene.addEdge(a, p);
+// p.pluckOutput().subscribe(function (value) {
+//     console.log("add output is", value);
+// });
+// const cube = scene.addObj('cube', [{name: 'color', default: 'red'}]);
+// const sphere = scene.addObj('sphere', [{name: 'color', default: 'blue'}]);
+// sphere.pluckOutput('color').subscribe(function (value) {
+//     console.log("sphere output is", value);
+// })
+// scene.addEdge({node: cube, prop: 'color'}, {node: sphere, prop: 'color'});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-//export { Scene } from './Scene';
-import { Scene } from './Scene';
+export { Scene } from './Scene';
+// import { Scene } from './Scene';
 
-const scene = new Scene();
-const a = scene.addConstant(2000);
-// const gen = scene.addOp('gen');
-// scene.addEdge(a, {node: gen, prop: 'delay'})
-const b = scene.addConstant(10);
-const p = scene.addOp('+');
-scene.addEdge(b, p);
-scene.addEdge(a, p);
-p.pluckOutput().subscribe(function (value) {
-    console.log("add output is", value);
-});
+// const scene = new Scene();
+// const a = scene.addConstant(2000);
+// // const gen = scene.addOp('gen');
+// // scene.addEdge(a, {node: gen, prop: 'delay'})
+// const b = scene.addConstant(10);
+// const p = scene.addOp('+');
+// scene.addEdge(b, p);
+// scene.addEdge(a, p);
+// p.pluckOutput().subscribe(function (value) {
+//     console.log("add output is", value);
+// });
 
-const cube = scene.addObj('cube', [{name: 'color', default: 'red'}]);
-const sphere = scene.addObj('sphere', [{name: 'color', default: 'blue'}]);
-sphere.pluckOutput('color').subscribe(function (value) {
-    console.log("sphere output is", value);
-})
-scene.addEdge({node: cube, prop: 'color'}, {node: sphere, prop: 'color'});
+// const cube = scene.addObj('cube', [{name: 'color', default: 'red'}]);
+// const sphere = scene.addObj('sphere', [{name: 'color', default: 'blue'}]);
+// sphere.pluckOutput('color').subscribe(function (value) {
+//     console.log("sphere output is", value);
+// })
+// scene.addEdge({node: cube, prop: 'color'}, {node: sphere, prop: 'color'});
